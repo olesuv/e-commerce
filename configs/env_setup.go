bc59c3295c0eb6df73e539e53f1d348ae05d9d04
Oleh <olegsuv.ukr@gmail.com>
2024-03-15 06:51:55 +0200

    mod: env setup error

--- configs/env_setup.go
+++ configs/env_setup.go
@@ -1,15 +1,13 @@
 package configs
 
-import (
-    "log"
-
-    "github.com/lpernett/godotenv"
-)
+import "github.com/lpernett/godotenv"
 
 
-func LoadEnv() {
-  err := godotenv.Load()
-  if err != nil {
-    log.Fatal("Error loading .env file")
-  }
+func LoadEnv() error {
+    err := godotenv.Load()
+    if err != nil {
+        return err
+    }
+    return nil
 }
+
