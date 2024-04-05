6d8a7a3f274f88126cf1efbfc4c01803e33cd4f1
plxgwalker <olegsuv.ukr@gmail.com>
2024-04-05 08:04:19 +0300

    mod: refactor

--- models/user_model.go
+++ models/user_model.go
@@ -2,12 +2,10 @@ package models
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-
 type User struct {
-	Id			primitive.ObjectID `bson:"_id"`
-	Name 		string 						 `bson:"name,omitempty"`
-	Email 	string 						 `bson:"email" validate:"required"` 
-	Phone 	string 						 `bson:"phone,omitempty"`
-	Orders	[]string					 `bson:"orders,omitempty"` 
+	Id     primitive.ObjectID `bson:"_id"`
+	Name   string             `bson:"name,omitempty"`
+	Email  string             `bson:"email" validate:"required"`
+	Phone  string             `bson:"phone,omitempty"`
+	Orders []string           `bson:"orders,omitempty"`
 }
-
