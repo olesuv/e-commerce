1bcdc2c64b5688576740b92b0d0f948b76abf159
plxgwalker <olegsuv.ukr@gmail.com>
2024-04-25 12:34:58 +0300

    add: header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,9 @@
 import "./App.css";
-import Congrats from "./components/Congrats";
 
 function App() {
   return (
     <div className="App">
       <h1>wassup React enjoyer</h1>
-      <Congrats />
     </div>
   );
 }
---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <div>
+      <ul>
+        <li>Sign In</li>
+        <li>Sign Up</li>
+      </ul>
+    </div>
+  );
+}
