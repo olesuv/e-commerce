5561bf2ae7959b689ce00234bc998afac9edae9c
plxgwalker <olegsuv.ukr@gmail.com>
2024-05-15 15:45:11 +0300

    refactor: centered status

--- client/src/components/order/OrderStatus.tsx
+++ client/src/components/order/OrderStatus.tsx
@@ -2,8 +2,8 @@ import { IOrderDetailsProps } from "../../../types/orderDetailsProps";
 
 export default function OrderStatus(props: IOrderDetailsProps) {
   return (
-    <div className="my-2 flex flex-wrap text-2xl font-semibold">
-      {props.orderData?.title}{" "}
+    <div className="my-2 flex flex-wrap items-center text-2xl font-semibold">
+      <div className="mr-2">{props.orderData?.title}</div>
       <span className="text-sm font-normal text-neutral-500">
         {props.orderData?.status === "Available" ? (
           <span className="text-green-500">✓ {props.orderData?.status}</span>
